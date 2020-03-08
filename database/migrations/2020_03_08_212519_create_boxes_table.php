3ca7014ffe54a2d5305ebf9ab6b03f4dc8d43a16
Ehsan Kiasaty <ehsan@kiasaty.com>
2020-03-08 21:50:41 +0330

    created database migrations

---
+++ database/migrations/2020_03_08_212519_create_boxes_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateBoxesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('boxes', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->unsignedBigInteger('creator_id');
+            $table->timestamps();
+
+            $table->foreign('creator_id')->references('id')->on('users')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('boxes');
+    }
+}
---
+++ database/migrations/2020_03_08_213619_create_cards_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCardsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('cards', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->unsignedBigInteger('box_id');
+            $table->string('front', 250);
+            $table->string('back', 1000);
+            $table->unsignedTinyInteger('level');
+            $table->timestamp('archived_at')->nullable();
+            $table->timestamps();
+
+            $table->foreign('box_id')->references('id')->on('boxes')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('cards');
+    }
+}
