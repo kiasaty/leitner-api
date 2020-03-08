d72c76874a26c53b033edb50a9d8c783de1d1ad1
Ehsan Kiasaty <ehsan@kiasaty.com>
2020-03-09 01:03:27 +0330

    set up authentication

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,106 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\User;
+use Firebase\JWT\JWT;
+use Illuminate\Http\Request;
+
+class AuthController extends Controller
+{
+    /**
+     * Authenticate the user and return the Token
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response JWT
+     */
+    public function authenticate(Request $request)
+    {
+        $credentials = $this->validateInput($request);
+
+        $user = $this->findUser($credentials);
+
+        $this->checkIfCredentialsAreCorrect($user, $credentials);
+
+        $jwt = $this->generateJWT($user);
+
+        return response()->json([
+            'token'     => $jwt,
+            'user'      => [
+                'id'            => $user->id,
+                'firstname'     => $user->firstname,
+                'lastname'      => $user->lastname,
+                'fullname'      => $user->firstname . ' ' . $user->lastname,
+                'username'      => $user->username,
+                'email'         => $user->email,
+                'profile_photo' => $user->profile_photo,
+            ]
+        ]);
+    }
+
+    /**
+     * Validates received credentials.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array validated credentials
+     */
+    private function validateInput(Request $request)
+    {
+        return $this->validate($request, [
+            'username'  => 'required|string',
+            'password'  => 'required|string'
+        ]);
+    }
+
+    /**
+     * Finds the user by username
+     *
+     * @param  array  $credentials
+     * @return \App\User
+     */
+    private function findUser($credentials)
+    {
+        return User::where('username', $credentials['username'])->first();
+    }
+    
+
+    /**
+     * Check if the credentials are correct.
+     *
+     * @param  \App\User $user
+     * @param  array $credentials
+     * @throws \Exception
+     */
+    private function checkIfCredentialsAreCorrect($user, $credentials)
+    {
+        $isPasswordCorrect = app('hash')->check($credentials['password'], $user->password);
+
+        if (is_null($user) || !$isPasswordCorrect) {
+            abort(422, 'Wrong Credentials!');
+        }
+    }
+
+    /**
+     * Generates the Json Web Token (JWT).
+     *
+     * @param  \App\User  $user
+     * @return string  jwt token
+     */
+    private function generateJWT($user)
+    {
+        // The application key is being used as secret key
+        $key = env('APP_KEY');
+
+        $payload = [
+            // Reserved claims:
+            'iss' => "leitner",             // Issuer of the token
+            'sub' => $user->id,             // Subject of the token
+            'iat' => time(),                // Time when JWT was issued. 
+            'exp' => time() + 60 * 60 * 12, // Expiration time (12 hours)
+
+            // Private claims:
+        ];
+        
+        return JWT::encode($payload, $key);
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -12,5 +12,7 @@
 */
 
 $router->get('/', function () use ($router) {
-    return $router->app->version();
+    return 'Memorize anything leitner way!';
 });
+
+$router->post('/login', 'AuthController@authenticate');
