aa13678ec75989dd1ec26a29df229785250c4ad0
Ehsan Kiasaty <ehsan@kiasaty.com>
2020-05-08 21:23:20 +0430

    fixed bug in increasing difficulty when a card demotes

--- app/Card.php
+++ app/Card.php
@@ -45,6 +45,6 @@ class Card extends Model
     {
         return $this->belongsToMany('App\User')
             ->as('progress')
-            ->withPivot(['level', 'deck_id', 'reviewed_at']);
+            ->withPivot(['level', 'deck_id', 'difficulty', 'reviewed_at']);
     }
 }
